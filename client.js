6cd62ecce237cb551731284b76b27434f29b18e5
Xai Hang <xaihang12@gmail.com>
2023-01-04 21:21:20 -0600

    declared addPurchase func

--- client.js
+++ client.js
@@ -1,11 +1,29 @@
 $(document).ready(readyNow);
 
 const budget = 25000; //my set budget range
+let purchase = [];
+
+function addPurchase() {
+    console.log('in addPurchase'); 
+    //get user input and create a new object:
+    let newPurchase = {
+        name: $('#purchaseNameIn').val(),
+        price: $('#purchasePriceIn').val()
+    }
+    //push the new purchase into the array:
+    purchases.push(newPurchase);
+    //empty inputs:
+    $('#purchaseNameIn').val(''); 
+    $('#purchasePriceIn').val('');
+    //calculate remainingBudget
+}
 
 function readyNow() {
     //display budget
-    //target budgetOut by id
+    //target budgetOut by id:
     let el = $('#budgetOut');
     el.empty();
     el.append(budget);
+    //handle click event:
+    $('#addPurchaseButton').on('click', addPurchase); 
 }
