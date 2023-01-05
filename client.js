9a7612db067c54218e6da0d0b4fd1638b2fcf3f5
Xai Hang <xaihang12@gmail.com>
2023-01-04 21:12:47 -0600

    declared budget variable + to readyNow func

--- client.js
+++ client.js
@@ -1,5 +1,11 @@
 $(document).ready(readyNow);
 
+const budget = 25000; //my set budget range
+
 function readyNow() {
-    console.log('JQ');
+    //display budget
+    //target budgetOut by id
+    let el = $('#budgetOut');
+    el.empty();
+    el.append(budget);
 }
